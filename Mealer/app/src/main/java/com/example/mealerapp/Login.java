4fd362c0e3991a7d8d2383c5569faf2011cc9e49
Bariq Talib <btali086@uottawa.ca>
2022-10-21 20:55:03 -0400

    Loggin in is finally working, using firebase

--- Mealer/app/src/main/java/com/example/mealerapp/Login.java
+++ Mealer/app/src/main/java/com/example/mealerapp/Login.java
@@ -1,23 +1,21 @@
 package com.example.mealerapp;
 import android.content.Intent;
+import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
 import com.google.android.gms.tasks.OnCompleteListener;
+
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-import android.widget.Toast;
 
 
 public class Login extends AppCompatActivity {
@@ -25,7 +23,7 @@ public class Login extends AppCompatActivity {
     Button theLoginBtn;
     TextView theCreateBtn;
     FirebaseAuth theAuth;
-    ProgressBar progressBar2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,15 +32,18 @@ public class Login extends AppCompatActivity {
 
         theEmail= findViewById(R.id.Email);
         thePassword= findViewById(R.id.password);
-        theLoginBtn= findViewById(R.id.loginBtn);
-        theCreateBtn= findViewById(R.id.textView4);
+        theLoginBtn= findViewById(R.id.log);
+        theCreateBtn= findViewById(R.id.createText);
         theAuth= FirebaseAuth.getInstance();
+
         theLoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 String email = theEmail.getText().toString().trim();
                 String password = thePassword.getText().toString().trim();
                 //Check if the user left the Email or password blank
+
+
                 if (TextUtils.isEmpty(password)){
                     thePassword.setError(" Please enter a password ");
                     return;
@@ -60,26 +61,21 @@ public class Login extends AppCompatActivity {
 
 
                 }
-//                if (TextUtils.isEmpty(phone)) {
-//                    mPhone.setError(" Please enter an Phone ");
-//                    return;
-//                }
 
-                progressBar2.setVisibility(View.VISIBLE);
+
 
                 theAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
-                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Login.this, "You are logged in! ", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
                         }else {
-                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                            progressBar2.setVisibility(View.GONE);
+                            Toast.makeText(Login.this, "Please verify your input! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
-                    }
-                    }
 
+                        }
+                }
                 });
 
 
